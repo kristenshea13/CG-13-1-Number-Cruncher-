c7fbd776d17de1a44b8d6c0cf921e294588bdab0
Kristen Shea <kstar61@live.com>
2018-10-03 22:16:03 -0500

    *

--- CG 13-1 (Number Cruncher)/Program.cs
+++ CG 13-1 (Number Cruncher)/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NumberUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CG_13_1__Number_Cruncher_
 {
@@ -6,7 +9,33 @@ namespace CG_13_1__Number_Cruncher_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create a program that has a static method to find the sum 
+            //    of all the even numbers in a list. This method should 
+            //    be in a class called NumberCruncher in its own class 
+            //library called NumberUtility.Within your console project, 
+            //    create a list with at least 10 integers and call your 
+            //    method on the list
+            
+
+
+            var Numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            
+
+
+
+
+
+
+
+            Console.WriteLine();
+            
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ ClassLibrary1/NumberCruncher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberUtility
+{
+    public class NumberCruncher
+    {
+
+        public int Number { get; set; }
+
+        public static List<int> Numbers { get; set; }
+
+        
+
+
+        public static int EvenSum()
+        {
+            var evenTotal = 0;
+            foreach (int number in Numbers)
+            {
+                if (number % 2 == 0)
+                    evenTotal = number + evenTotal;
+
+
+                else
+                    continue;
+            }
+
+            return evenTotal;
+
+        }
+
+        
+            
+        
+
+
+       
+
+    }
+}
