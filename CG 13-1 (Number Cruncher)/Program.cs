6a63d13c1fe7fb6acd9007e83472a0160c00314d
Kristen Shea <kstar61@live.com>
2018-10-03 21:00:12 -0500

    Add project files.

---
+++ CG 13-1 (Number Cruncher)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_13_1__Number_Cruncher_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
