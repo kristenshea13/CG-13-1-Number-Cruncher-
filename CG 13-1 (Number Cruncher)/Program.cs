c29fea466630f8896739a8390a0a6a6f45b432e7
Kristen Shea <kstar61@live.com>
2018-10-04 19:59:49 -0500

    updated comments

--- CG 13-1 (Number Cruncher)/Program.cs
+++ CG 13-1 (Number Cruncher)/Program.cs
@@ -9,19 +9,17 @@ namespace CG_13_1__Number_Cruncher_
     {
         static void Main(string[] args)
         {
-            //Create a program that has a static method to find the sum 
-            //    of all the even numbers in a list. This method should 
-            //    be in a class called NumberCruncher in its own class 
-            //library called NumberUtility.Within your console project, 
-            //    create a list with at least 10 integers and call your 
-            //    method on the list
-            
+            //I created a set list, but can create list from user-entered integers as well
+
 
 
             var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
+
+            //create variable for value from method in class
             int sum = NumberCruncher.EvenSum(numbers);
 
+            //print to console in a string
             Console.WriteLine("The sum of the even numbers in a list from 1-10: {0}", sum);
 
 
--- ClassLibrary1/NumberCruncher.cs
+++ ClassLibrary1/NumberCruncher.cs
@@ -9,10 +9,13 @@ namespace NumberUtility
         
 
         
+        //NumberUtility class library with a class called NumberCruncher
 
         
-
-
+        
+         //method to total the sum of all even numbers in a list
+         //checks number to see if it is even then adds together
+         //if odd, number is skipped and loop continues
         public static int EvenSum(List<int> numbers)
         {
             var evenTotal = 0;
